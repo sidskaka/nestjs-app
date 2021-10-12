a3e78694961efb59621c8c5f2764ccd9e96d424a
sidskaka <sidyka01@gmail.com>
2021-10-12 15:09:21 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+require('dotenv').config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8000);
+  const port = process.env.PORT || 4000
+  await app.listen(port);
 }
 bootstrap();
